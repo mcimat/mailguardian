891f145b8caf87c07e0c838921237cebeff04cb0
mcimat <mateomcintyre@gmail.com>
2023-10-08 22:38:45 -0500

    added some questions, need to think of more.

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,16 +1,39 @@
 function Questions() {
-    return (
-        <section>
-        <div className="container">
-          <div className="container-title">
-            <h1>Frequently Asked Questions</h1>
+  return (
+    <section>
+      <div className="container">
+        <div className="container-title">
+          <h1>Frequently Asked Questions</h1>
+        </div>
+        <div className="container-questions">
+          <div className="questions-content">
+            <h2>Do we retain information about emails searched for?</h2>
+            <p>Rest assured, we have implemented stringent measures to protect your privacy. As a result, we do not retain any records of the emails you've searched for. Your search activities are treated with the utmost confidentiality and are not stored in any form, ensuring that your experience on our platform remains completely private and secure.</p>
           </div>
-          <div className="container-text">
-            <p>Take a moment to examine the present allocation of monthly Searches and Verifications accessible for your account. It's important to be aware that this allocation is renewed each month, ensuring that you consistently have a refreshed quota of Searches and Verifications at your disposal to meet your needs and objectives.</p>
+          <div className="questions-content">
+            <h2>TITLE</h2>
+            <p>HELLO</p>
+          </div>
+          <div className="questions-content">
+            <h2>TITLE</h2>
+            <p>HELLO</p>
+          </div>
+          <div className="questions-content">
+            <h2>TITLE</h2>
+            <p>HELLO</p>
+          </div>
+          <div className="questions-content">
+            <h2>How does MailGuardian work?</h2>
+            <p>MailGuardian is a site run on the Hunter API. </p>
+          </div>
+          <div className="questions-content">
+            <h2>Does MailGuardian work on mobile devices?</h2>
+            <p>Absolutely, MailGuardian is designed to seamlessly function on mobile devices. It operates as a web-based tool, eliminating the need for any installations. All that's required is an internet connection, allowing you to effortlessly access MailGuardian from any web browser on your mobile device.</p>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default Questions;
